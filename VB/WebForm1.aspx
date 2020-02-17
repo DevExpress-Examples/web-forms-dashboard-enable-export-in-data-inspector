b59df39ecae48837662227e24417ee06ccbbd006
zakhodiaeva.margarita <margarita.zakhodiaeva@devexpress.com>
2020-02-17 15:25:40 +0300

    add VB solution

---
+++ VB/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="vb" AutoEventWireup="true" CodeBehind="WebForm1.aspx.vb" Inherits="Lesson3.WebForm1" %>
+
+<%@ Register assembly="DevExpress.Dashboard.v19.2.Web.WebForms, Version=19.2.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.DashboardWeb" tagprefix="dx" %>
+
+<%@ Register assembly="DevExpress.Web.v19.2, Version=19.2.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.Web" tagprefix="dx" %>
+
+<%@ Register assembly="DevExpress.Dashboard.v19.2.Web.WebForms, Version=19.2.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.DashboardWeb" tagprefix="dx" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+	<script src="../Scripts/jszip.min.js"></script>
+	<script src="../Scripts/beforeRender.js"></script>
+</head>
+<body >
+	<form id="form1" runat="server">
+	<dx:ASPxDashboard ID="ASPxDashboard1" runat="server"
+		ClientInstanceName="ASPxDashboard1"
+		AllowInspectAggregatedData="true"
+		AllowInspectRawData="true"
+					IncludeDashboardIdToUrl = "True"
+					IncludeDashboardStateToUrl = "True"
+		OnConfigureDataConnection="ASPxDashboard1_ConfigureDataConnection"
+		ClientSideEvents-BeforeRender="onBeforeRender">
+			</dx:ASPxDashboard>
+	</form>
+</body>
+</html>
---
+++ VB/WebForm1.aspx.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardWeb
+Imports DevExpress.DataAccess.Web
+Imports System
+Imports System.Web.Hosting
+
+Namespace Lesson3
+	Partial Public Class WebForm1
+		Inherits System.Web.UI.Page
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+			Dim newDashboardStorage As New DashboardFileStorage(HostingEnvironment.MapPath("~/App_Data/Dashboards"))
+			ASPxDashboard1.SetDashboardStorage(newDashboardStorage)
+
+			ASPxDashboard1.SetConnectionStringsProvider(New ConfigFileConnectionStringsProvider())
+			Dim dataSourceStorage As New DataSourceInMemoryStorage()
+
+			Dim extractDataSource As New DashboardExtractDataSource("Extract Data Source")
+			extractDataSource.Name = "Extract Data Source"
+			dataSourceStorage.RegisterDataSource("extractDataSource", extractDataSource.SaveToXml())
+			ASPxDashboard1.SetDataSourceStorage(dataSourceStorage)
+		End Sub
+
+		Protected Sub ASPxDashboard1_ConfigureDataConnection(ByVal sender As Object, ByVal e As ConfigureDataConnectionWebEventArgs)
+			If e.DataSourceName.Contains("Extract Data Source") Then
+				Dim extractParams As New ExtractDataSourceConnectionParameters()
+				extractParams.FileName = HostingEnvironment.MapPath("~/App_Data/SalesPersonExtract.dat")
+				e.ConnectionParameters = extractParams
+			End If
+		End Sub
+	End Class
+End Namespace
